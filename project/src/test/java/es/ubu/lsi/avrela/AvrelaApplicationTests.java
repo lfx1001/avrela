22bb70f9fc5415026993ee6b965b1e94552e9636
lfx1001 <lfx1001@alu.ubu.es>
2022-10-16 18:24:42 +0200

    Add initial project with minimal dependencies

---
+++ project/src/test/java/es/ubu/lsi/avrela/AvrelaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ubu.lsi.avrela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvrelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
