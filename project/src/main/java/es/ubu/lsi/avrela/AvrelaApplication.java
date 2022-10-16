4dba02327b89a12ae562f5fa8686e912c990bafe
lfx1001 <lfx1001@alu.ubu.es>
2022-10-16 18:28:16 +0200

    17 add initial code project with minimal dependencies (#21)
    * WIP: add code formatter
    
    * Add initial project with minimal dependencies

---
+++ project/src/main/java/es/ubu/lsi/avrela/AvrelaApplication.java
@@ -0,0 +1,13 @@
+package es.ubu.lsi.avrela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvrelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvrelaApplication.class, args);
+	}
+
+}
