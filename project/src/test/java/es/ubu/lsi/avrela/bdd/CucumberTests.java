75ebe409f3327f9910c0d8ac3fa14b2857ff3ae1
lfx1001 <lfx1001@alu.ubu.es>
2022-11-01 18:18:45 +0100

    WIP: Add first Cucumber test

--- project/src/test/java/es/ubu/lsi/avrela/bdd/CucumberTests.java
+++ project/src/test/java/es/ubu/lsi/avrela/bdd/CucumberTests.java
@@ -1,6 +1,8 @@
 package es.ubu.lsi.avrela.bdd;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
@@ -11,6 +13,8 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("es/ubu/lsi/avrela/bdd")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "es.ubu.lsi.avrela.bdd")
-public class CucumberConfig {
+@ConfigurationParameter(key= PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "true")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class CucumberTests {
 
 }
