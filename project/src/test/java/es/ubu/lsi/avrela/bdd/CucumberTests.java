d92c5a956d28bca27e26f0affd396f4cc622a780
lfx1001 <lfx1001@alu.ubu.es>
2022-11-01 18:24:15 +0100

    WIP: ensure surefire plugin run both junit and cucumber tests

--- project/src/test/java/es/ubu/lsi/avrela/bdd/CucumberTests.java
+++ project/src/test/java/es/ubu/lsi/avrela/bdd/CucumberTests.java
@@ -4,6 +4,7 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME;
 
+import io.cucumber.junit.platform.engine.Cucumber;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
