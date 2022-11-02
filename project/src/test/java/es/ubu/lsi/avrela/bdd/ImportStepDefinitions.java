238ca31aa8a6947e6fc7fd5c884b2fde2a581f79
lfx1001 <lfx1001@alu.ubu.es>
2022-11-02 07:45:13 +0100

    WIP: repository import feature

---
+++ project/src/test/java/es/ubu/lsi/avrela/bdd/ImportStepDefinitions.java
@@ -0,0 +1,24 @@
+package es.ubu.lsi.avrela.bdd;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ImportStepDefinitions {
+
+  @Given("repository can be fetched")
+  public void fetchRepository() {
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("I try to import the repository")
+  public void importRepository() {
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("repository project management info should be accessed locally")
+  public void getRepository() {
+    throw new io.cucumber.java.PendingException();
+  }
+
+}
