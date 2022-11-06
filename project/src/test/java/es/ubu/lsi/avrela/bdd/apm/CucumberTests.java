daa4663e074d51587bd38a0b3a45f6cd294ca771
lfx1001 <lfx1001@alu.ubu.es>
2022-11-06 20:05:56 +0100

    Restore BDD tests execution

--- project/src/test/java/es/ubu/lsi/avrela/bdd/apm/CucumberTests.java
+++ project/src/test/java/es/ubu/lsi/avrela/bdd/apm/CucumberTests.java
@@ -4,6 +4,7 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME;
 
+import io.cucumber.junit.platform.engine.Cucumber;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
@@ -15,6 +16,6 @@ import org.junit.platform.suite.api.Suite;
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "es.ubu.lsi.avrela.bdd")
 @ConfigurationParameter(key= PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "true")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-public class Cucumber {
+public class CucumberTests {
 
 }
