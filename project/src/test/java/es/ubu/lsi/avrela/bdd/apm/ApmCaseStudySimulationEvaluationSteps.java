433cd26ab6289a3018d3c1846b30424928c10286
lfx1001 <lfx1001@alu.ubu.es>
2022-12-08 19:18:10 +0100

    WIP: finish team work evaluation

--- project/src/test/java/es/ubu/lsi/avrela/bdd/apm/ApmCaseStudySimulationEvaluationSteps.java
+++ project/src/test/java/es/ubu/lsi/avrela/bdd/apm/ApmCaseStudySimulationEvaluationSteps.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 
 @Slf4j
 public class ApmCaseStudySimulationEvaluationSteps {
@@ -28,6 +29,7 @@ public class ApmCaseStudySimulationEvaluationSteps {
   Integer simulationParticipants;
 
   GitHubHistoricalApmDataRepository apmDataRepository;
+  private Integer actualTeamWorkRubricValue = 0;
 
   @Given("a case study with repo owner {string}, name {string} and time period {zoneddatetime} {zoneddatetime}")
   public void aCaseStudyWithRepoOwnerNameAndTimePeriod(String repoOwner, String repoName, ZonedDateTime startAt,
@@ -37,14 +39,15 @@ public class ApmCaseStudySimulationEvaluationSteps {
     GitHubMilestoneMapper milestoneMapper = GitHubMilestoneMapper.build();
     GitHubSprintFinder sprintFinder = new GitHubSprintFinder(gitHubClient, milestoneMapper);
     apmDataRepository = new GitHubHistoricalApmDataRepository(sprintFinder);
+    //Uncomment lines below for case study evaluation
     //Fetch
-    caseStudy = apmDataRepository.findByRepoOwnerAndRepoNameAndSprintDueBetween(repoOwner, repoName, startAt, endAt);
+    // caseStudy = apmDataRepository.findByRepoOwnerAndRepoNameAndSprintDueBetween(repoOwner, repoName, startAt, endAt);
   }
 
   @And("a simulation with repo owner {string}, name {string}, time period {zoneddatetime} {zoneddatetime} and {int} participant\\(s)")
   public void aSimulationWithRepoOwnerNameAndTimePeriod(String repoOwner, String repoName, ZonedDateTime startAt,
       ZonedDateTime endAt, Integer simulationParticipants) {
-    simulationParticipants = simulationParticipants;
+    this.simulationParticipants = simulationParticipants;
     simulation = apmDataRepository.findByRepoOwnerAndRepoNameAndSprintDueBetween(repoOwner, repoName, startAt, endAt);
   }
 
@@ -64,13 +67,17 @@ public class ApmCaseStudySimulationEvaluationSteps {
     }
     //Evaluate value
     //1.Get value
-    Double teamWork = 100D;
-    Integer scaleValuePos = 0, scaleValueCurrent = 0;
+    Double teamWorkNum = 100d *  simulation.filterIssues(
+        Issue.participantsGreaterThanOrEqual(simulationParticipants)).size();
+    Double teamWordDen = simulation.countIssues().doubleValue();
+    Double teamWork = teamWorkNum / teamWordDen;
+    Integer scaleValueCurrent = 0;
     Boolean finish = false;
+    //2. Get scale value
     while(!finish){
       if(teamWork >= teamWorkCriteriaScale.get(scaleValueCurrent)){
         if(Double.MIN_VALUE != teamWorkCriteriaScale.get(scaleValueCurrent)){
-          scaleValuePos = scaleValueCurrent;
+          actualTeamWorkRubricValue = scaleValueCurrent;
           if (scaleValueCurrent == teamWorkCriteriaScale.size()-1 ){
             finish = true;
           }else{
@@ -84,24 +91,30 @@ public class ApmCaseStudySimulationEvaluationSteps {
         finish = true;
       }
     }
-    log.debug("Calculated pos for [{}] value is [{}]", teamWork, scaleValuePos);
+    log.debug("Calculated pos for [{}] value is [{}]", teamWork, actualTeamWorkRubricValue);
 
   }
 
   @When("I apply the rubric")
   public void iApplyTheRubric() {
     //throw new io.cucumber.java.PendingException();
-    simulation.getParticipants().forEach(
-        user -> log.debug("User [{}]", user)
-    );
-    log.debug("Number of issues is [{}]", simulation.countIssues());
-    log.debug("Number of issues that match predicate  [{}]", simulation.filterIssues(
-        Issue.participantsGreaterThanOrEqual(1)).size());
   }
 
   @Then("rubric score should be")
   public void rubricScoreShouldBe(DataTable dataTable) {
-    //throw new io.cucumber.java.PendingException();
+    List<Map<String, String>> rows = dataTable.asMaps(String.class, String.class);
+    //Process team work criteria
+    Map<String, String>  teamWorkEvaluationRow = rows.get(0);
+    String[] ratingScaleValues = {"0", "1", "2"};
+    Integer expectedTeamWorkRubricValue = 0;
+    for(String ratingScaleValue: ratingScaleValues){
+      if ("X".equals(teamWorkEvaluationRow.get(ratingScaleValue))){
+        break;
+      }
+      expectedTeamWorkRubricValue++;
+    }
+
+    Assertions.assertEquals(expectedTeamWorkRubricValue, actualTeamWorkRubricValue);
   }
 
 }
