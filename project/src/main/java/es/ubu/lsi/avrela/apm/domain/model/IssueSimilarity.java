a4702c6d5a51d72b0cf9897ffbdb239bae95ad65
lfx1001 <lfx1001@alu.ubu.es>
2023-01-14 20:32:22 +0100

    WIP Improve similarity calculation readibility

--- project/src/main/java/es/ubu/lsi/avrela/apm/domain/model/IssueSimilarity.java
+++ project/src/main/java/es/ubu/lsi/avrela/apm/domain/model/IssueSimilarity.java
@@ -1,11 +1,17 @@
 package es.ubu.lsi.avrela.apm.domain.model;
 
+import java.util.EnumMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class IssueSimilarity {
 
-  public static double calculate(Issue a, Issue b) {
+  public enum Feature {
+    LABELS,
+    STATE
+  }
+
+  public static double calculate(Issue a, Issue b, EnumMap<Feature, Double> featureWeights) {
     // Calculate the intersection and union between the two sets of labels
     Set<String> labelIntersection = new HashSet<>(a.getLabels());
     labelIntersection.retainAll(b.getLabels());
@@ -17,12 +23,10 @@ public class IssueSimilarity {
     // Calculate the Jaccard similarity using the formula:
     // (size of intersection) / (size of union)
     double labelSimilarity = (double) labelIntersectionSize / labelUnionSize;
-    double labelSimilarityWeight = 1.0;
 
     int stateSimilarity = ( a.getState() == b.getState()) ? 1 : 0;
-    double stateSimilarityWeight = 1.0;
 
-    return ((labelSimilarity*labelSimilarityWeight)
-        +(stateSimilarity*stateSimilarityWeight))/(labelSimilarityWeight+stateSimilarityWeight);
+    return ((labelSimilarity*featureWeights.get(Feature.LABELS))
+        +(stateSimilarity*featureWeights.get(Feature.STATE)))/featureWeights.values().stream().mapToDouble(Double::doubleValue).sum();
   }
 }
--- project/src/test/java/es/ubu/lsi/avrela/apm/domain/model/IssueSimilarityTest.java
+++ project/src/test/java/es/ubu/lsi/avrela/apm/domain/model/IssueSimilarityTest.java
@@ -1,5 +1,7 @@
 package es.ubu.lsi.avrela.apm.domain.model;
 
+import es.ubu.lsi.avrela.apm.domain.model.IssueSimilarity.Feature;
+import java.util.EnumMap;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,7 +14,7 @@ public class IssueSimilarityTest {
 
     Issue b = a;
 
-    double result = IssueSimilarity.calculate(a,b);
+    double result = IssueSimilarity.calculate(a,b, getFeatureWeights());
 
     Assertions.assertEquals(1.0, result, "Should be totally similar");
 
@@ -31,7 +33,7 @@ public class IssueSimilarityTest {
         .labels(List.of("label3", "label4"))
         .build();
 
-    double result = IssueSimilarity.calculate(a,b);
+    double result = IssueSimilarity.calculate(a,b,getFeatureWeights());
 
     Assertions.assertEquals(0.0, result, "Should be totally different");
 
@@ -45,6 +47,13 @@ public class IssueSimilarityTest {
         .hasTaskList(true)
         .labels(List.of("label1", "label2"))
         .build();
+  }
+
+  private static EnumMap<IssueSimilarity.Feature, Double> getFeatureWeights() {
+    EnumMap<IssueSimilarity.Feature, Double> result = new EnumMap<>(IssueSimilarity.Feature.class);
+    result.put(Feature.LABELS, 1.0);
+    result.put(Feature.STATE, 1.0);
+    return result;
   }
 
 }
