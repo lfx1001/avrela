d662835e5b34ed6dfd4d6d706ba0e7ffce3846cd
lfx1001 <lfx1001@alu.ubu.es>
2023-03-04 21:08:29 +0100

    Agile project management case study simulation evaluation input page (#109)
    * WIP Add basic  home navigation
    
    * WIP polish APM evaluation page
    
    * WIP add APM case study simulation input form
    
    * WIP post info test
    
    * WIP bind sample fields
    
    * WIP deal with dates
    
    * WIP deal with select html control
    
    * WIP bind issue similarity function

---
+++ project/src/main/java/es/ubu/lsi/avrela/css/adapter/web/ApmController.java
@@ -0,0 +1,49 @@
+package es.ubu.lsi.avrela.css.adapter.web;
+
+import es.ubu.lsi.avrela.apm.adapter.web.WebHistoricalApmData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Slf4j
+public class ApmController {
+
+  @GetMapping("/css-apm")
+  public String index(Model model){
+    WebApmCaseStudySimulation result = WebApmCaseStudySimulation.builder()
+        .caseStudy( WebHistoricalApmData.builder()
+            .repoOwner("davidmigloz")
+            .repoName("go-bees")
+            .stringifyStartAt("2017-01-25")
+            .stringifyEndAt("2017-01-25")
+            .build())
+        .simulation( WebHistoricalApmData.builder()
+            .repoOwner("davidmigloz")
+            .repoName("go-bees")
+            .stringifyStartAt("2017-01-25")
+            .stringifyEndAt("2017-01-25")
+            .build())
+        .participants(1)
+        .issueSimilarityFunctionConfig(
+            WebIssueSimilarityFunctionConfig.builder()
+                .labelWeight(1.0)
+                .stateWeight(1.0)
+                .issueNameWeight(1.0)
+                .build()
+        )
+        .build();
+    model.addAttribute("webApmCaseStudySimulation", result);
+    return "apm/index";
+  }
+
+  @PostMapping("/css-apm")
+  public String create(@ModelAttribute WebApmCaseStudySimulation sim, Model model){
+    model.addAttribute("webApmCaseStudySimulation", sim);
+    return "apm/index";
+  }
+
+}
---
+++ project/src/main/java/es/ubu/lsi/avrela/css/adapter/web/WebApmCaseStudySimulation.java
@@ -0,0 +1,19 @@
+package es.ubu.lsi.avrela.css.adapter.web;
+
+import es.ubu.lsi.avrela.apm.adapter.web.WebHistoricalApmData;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class WebApmCaseStudySimulation {
+
+  private WebHistoricalApmData caseStudy;
+
+  private WebHistoricalApmData simulation;
+
+  private Integer participants;
+
+  private WebIssueSimilarityFunctionConfig issueSimilarityFunctionConfig;
+
+}
