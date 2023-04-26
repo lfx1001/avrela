74e03acc8efa7f04ea4b40cf86adf4cb10046b54
lfx1001 <lfx1001@alu.ubu.es>
2023-04-26 21:46:58 +0200

    Add basic scm  UI resources (#125)

---
+++ project/src/main/java/es/ubu/lsi/avrela/css/adapter/web/WebScmCaseStudySimulation.java
@@ -0,0 +1,21 @@
+package es.ubu.lsi.avrela.css.adapter.web;
+
+import es.ubu.lsi.avrela.apm.adapter.web.WebHistoricalApmData;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class WebScmCaseStudySimulation {
+
+  private WebHistoricalApmData caseStudy;
+
+  private WebHistoricalApmData simulation;
+
+  private Integer participants;
+
+  private WebIssueSimilarityFunctionConfig issueSimilarityFunctionConfig;
+
+  private WebRubricEvaluation  rubricEvaluation;
+
+}
