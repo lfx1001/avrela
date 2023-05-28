e9d559b7315969ddb292ed72281ad9cfd823cc5f
lfx1001 <lfx1001@alu.ubu.es>
2023-05-28 21:56:31 +0200

    First SCM evaluation analysis must be null

--- project/src/main/java/es/ubu/lsi/avrela/css/util/ScmCssDataGenerator.java
+++ project/src/main/java/es/ubu/lsi/avrela/css/util/ScmCssDataGenerator.java
@@ -20,7 +20,7 @@ public class ScmCssDataGenerator {
         .commitSimilarityFunctionConfig(
             getSimilarityFunctionConfig()
         )
-        .rubricEvaluation(getScmWebRubricEvaluation())
+        .rubricEvaluation(null)
         .build();
   }
 
